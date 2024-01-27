13d85957c459474442b983d2d70d269bc6438cb5
SaiyanDionysus <pusika97@gmail.com>
2024-01-27 13:57:55 +0300

    secondcommit

--- nodejs_POST/app.js
+++ nodejs_POST/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
-
+   
 const app = express();
 
 const urlencodedParser = express.urlencoded({extended: false});
-
-app.get("/", function(_, response){
+  
+app.get("/", function (_, response) {
     response.sendFile(__dirname + "/index.html");
 });
-app.post("/", urlendodedParser, function (request, response) {
+app.post("/", urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
     response.send(`${request.body.userName} - ${request.body.userAge}`);
 });
-
+   
 app.listen(3000, ()=>console.log("Сервер запущен..."));
